ba3fe3833d5307ab159d2f08d3f8dc30f978e820
deric2019 <deric@student.chalmers.se>
2022-06-20 20:09:21 +0200

    Completed project

--- Python_Crash_Course/Part_II_Projects/Alien_Invasion/alien.py
+++ Python_Crash_Course/Part_II_Projects/Alien_Invasion/alien.py
@@ -2,18 +2,18 @@ import pygame
 
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
-    # A class to represent a singe alien in the fleet.
+    """A class to represent a single alien in the fleet."""
 
     def __init__(self, ai_settings, screen):
-
         """Initialize the alien and set its starting position."""
-        super(Alien, self).__init__()
+        super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load_basic('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
@@ -24,12 +24,13 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def blitme(self):
-        # Draw the alien at its current location
+        """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
 
     def update(self):
         """Move the alien right or left."""
-        self.x += self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
         self.rect.x = self.x
 
     def check_edges(self):
@@ -39,4 +40,3 @@ class Alien(Sprite):
             return True
         elif self.rect.left <= 0:
             return True
-
--- Python_Crash_Course/Part_II_Projects/Alien_Invasion/bullet.py
+++ Python_Crash_Course/Part_II_Projects/Alien_Invasion/bullet.py
@@ -2,16 +2,19 @@ import pygame
 
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
-    # A class to manage bullets fired from the ship
+    """A class to manage bullets fired from the ship"""
 
     def __init__(self, ai_settings, screen, ship):
-        # Create a bullet object at the ship's current position
-        super(Bullet, self).__init__()
+        """Create a bullet object at the ship's current position."""
+        # super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
-        # Create a bullet rect at (0, 0) and then set correct position
-        self.rect = pygame.Rect(0,0, ai_settings.bullet_width, ai_settings.bullet_height)
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+                                ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
 
@@ -22,14 +25,12 @@ class Bullet(Sprite):
         self.speed_factor = ai_settings.bullet_speed_factor
 
     def update(self):
-        # Move the bullet up the screen.
-        # Update the decimal postion of the bullet.
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
         self.y -= self.speed_factor
-
         # Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
-        # Draw the bullet to the screen
+        """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
-
